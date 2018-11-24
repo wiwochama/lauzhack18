f771842b56997580c59b550d23425af8bad229c7
Mathieu <mathieu.ducroux@epfl.ch>
2018-11-24 20:15:46 +0100

    remove .idea?

---
+++ mobile/src/main/java/com/example/wiwochama/myousicrun/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.wiwochama.myousicrun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
