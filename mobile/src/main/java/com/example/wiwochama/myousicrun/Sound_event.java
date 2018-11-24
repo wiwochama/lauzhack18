f7943d775845f539bb0da3689807dc3f05e3c754
wreise <reisewojtus@gmail.com>
2018-11-24 20:33:11 +0100

    Fixed compilation due to Sound

--- mobile/src/main/java/com/example/wiwochama/myousicrun/Sound_event.java
+++ mobile/src/main/java/com/example/wiwochama/myousicrun/Sound_event.java
@@ -2,18 +2,20 @@ package com.example.wiwochama.myousicrun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import soundPool;
+//import soundPool;
 
-global int integration_time = 5;
+class Globals {
+    public static int integration_time = 5;
+}
 
-public class Sound_evet {
+public class Sound_event {
 
     public void sound_start() {
 
     }
 
     public void test(){
-        System.out.print("yes")
+        System.out.print("yes");
     }
 }
 
