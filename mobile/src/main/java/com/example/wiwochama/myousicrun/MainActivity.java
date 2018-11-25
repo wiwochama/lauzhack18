e747b70d735e96064083d5ced20b22636ca3628c
WilliamCappelletti <cappelletti.william@gmail.com>
2018-11-25 10:53:16 +0100

    Music pace fixed

--- mobile/src/main/java/com/example/wiwochama/myousicrun/MainActivity.java
+++ mobile/src/main/java/com/example/wiwochama/myousicrun/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     private HeartRateModel heartRateModel;
 
-    private static int musicPace = 120;
+    private static double musicPace = 120;
     private static float bass_volume = (float) (1 + Math.log(hrObjective / 220));
     private static float high_volume = (float) (Math.log(220 / stepObjective));
 
@@ -76,47 +76,45 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
 
-            if (!streaming) {
-                seconds++;
+            seconds++;
 
-                // scheduled another events to be in 10 seconds later
-                handler.postDelayed(PeriodicUpdate, 1000);
-                // below is whatever you want to do
+            // scheduled another events to be in 10 seconds later
+            handler.postDelayed(PeriodicUpdate, 1000);
+            // below is whatever you want to do
 
-                double HR_now = getHeartRate();
-                HRs.add(HR_now);
+            double HR_now = getHeartRate();
+            HRs.add(HR_now);
 
-                double step_now = getStepPerMin();
-                steps.add(step_now);
-                double HR_old = HRs.remove();
-                double step_old = steps.remove();
+            double step_now = getStepPerMin();
+            steps.add(step_now);
+            double HR_old = HRs.remove();
+            double step_old = steps.remove();
 
-                //Volume Computation
-                HR_mean += (HR_now - HR_old) / integration_time;
-                bass_volume = (float) (1 + Math.log(HR_mean));
+            //Volume Computation
+            HR_mean += (HR_now - HR_old) / integration_time;
+            bass_volume = (float) (1 + Math.log(HR_mean));
 
-                step_mean += (step_now - step_old) / integration_time;
-                high_volume = (float) (Math.log(220 / step_mean));
+            step_mean += (step_now - step_old) / integration_time;
+            high_volume = (float) (Math.log(220 / step_mean));
 
 
-                //Music Pace
-                PlaybackParams plbParam = new PlaybackParams();
-                plbParam.setSpeed((float) (stepObjective / musicPace));
-                //            plbParam.setSpeed((float) (pace / absolutePace));
+            //Music Pace
+            PlaybackParams plbParam = new PlaybackParams();
+            plbParam.setSpeed((float) (stepObjective / musicPace));
+            //            plbParam.setSpeed((float) (pace / absolutePace));
 
-                //Music Transformation :D
-                if (bass_player != null) {
-                    bass_player.setPlaybackParams(plbParam);
-                    bass_player.setVolume(bass_volume, bass_volume);
+            //Music Transformation :D
+            if (bass_player != null) {
+                bass_player.setPlaybackParams(plbParam);
+                bass_player.setVolume(bass_volume, bass_volume);
 
-                }
-                if (high_player != null) {
-                    high_player.setPlaybackParams(plbParam);
-                    high_player.setVolume(high_volume, high_volume);
-                }
-                if (mid_player != null) {
-                    mid_player.setPlaybackParams(plbParam);
-                }
+            }
+            if (high_player != null) {
+                high_player.setPlaybackParams(plbParam);
+                high_player.setVolume(high_volume, high_volume);
+            }
+            if (mid_player != null) {
+                mid_player.setPlaybackParams(plbParam);
             }
         }
     };
@@ -125,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         playPause(findViewById(R.id.imageButtonPlayPause));
         initialize_queues();
     }
