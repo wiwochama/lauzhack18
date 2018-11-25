f52800b36510202eb8e0c5cb080305f23557973d
WilliamCappelletti <cappelletti.william@gmail.com>
2018-11-25 07:56:51 +0100

    HR model more realistic

--- mobile/src/main/java/com/example/wiwochama/myousicrun/MainActivity.java
+++ mobile/src/main/java/com/example/wiwochama/myousicrun/MainActivity.java
@@ -37,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         integration_time = 5;
     }
 
+    private static int seconds = 0;
+
     private double heartRate = 60;
     private double stepPerMin = 160;
     private double speed = 10;
@@ -74,43 +76,47 @@ public class MainActivity extends AppCompatActivity {
     private Runnable PeriodicUpdate = new Runnable() {
         @Override
         public void run() {
-            // scheduled another events to be in 10 seconds later
-            handler.postDelayed(PeriodicUpdate, 1000);
-            // below is whatever you want to do
+            if (!streaming) {
+                seconds++;
 
-            double HR_now = getHeartRate();
-            HRs.add(HR_now);
+                // scheduled another events to be in 10 seconds later
+                handler.postDelayed(PeriodicUpdate, 1000);
+                // below is whatever you want to do
 
-            double step_now = getStepPerMin();
-            steps.add(step_now);
-            double HR_old = HRs.remove();
-            double step_old = steps.remove();
+                double HR_now = getHeartRate();
+                HRs.add(HR_now);
 
-            //Volume Computation
-            HR_mean += (HR_now - HR_old) / integration_time;
-            bass_volume = (float) (1 + Math.log(HR_mean));
+                double step_now = getStepPerMin();
+                steps.add(step_now);
+                double HR_old = HRs.remove();
+                double step_old = steps.remove();
 
-            step_mean += (step_now - step_old) / integration_time;
-            high_volume = (float) (Math.log(220 / step_mean));
+                //Volume Computation
+                HR_mean += (HR_now - HR_old) / integration_time;
+                bass_volume = (float) (1 + Math.log(HR_mean));
+
+                step_mean += (step_now - step_old) / integration_time;
+                high_volume = (float) (Math.log(220 / step_mean));
 
 
-            //Music Pace
-            PlaybackParams plbParam = new PlaybackParams();
-            plbParam.setSpeed((float) stepObjective / 160);
-//            plbParam.setSpeed((float) (pace / absolutePace));
+                //Music Pace
+                PlaybackParams plbParam = new PlaybackParams();
+                plbParam.setSpeed((float) stepObjective / 160);
+                //            plbParam.setSpeed((float) (pace / absolutePace));
 
-            //Music Transformation :D
-            if (bass_player != null && streaming) {
-                bass_player.setPlaybackParams(plbParam);
-                bass_player.setVolume(bass_volume, bass_volume);
+                //Music Transformation :D
+                if (bass_player != null) {
+                    bass_player.setPlaybackParams(plbParam);
+                    bass_player.setVolume(bass_volume, bass_volume);
 
-            }
-            if (high_player != null && streaming) {
-                high_player.setPlaybackParams(plbParam);
-                high_player.setVolume(high_volume, high_volume);
-            }
-            if (mid_player != null && streaming) {
-                mid_player.setPlaybackParams(plbParam);
+                }
+                if (high_player != null) {
+                    high_player.setPlaybackParams(plbParam);
+                    high_player.setVolume(high_volume, high_volume);
+                }
+                if (mid_player != null) {
+                    mid_player.setPlaybackParams(plbParam);
+                }
             }
         }
     };
@@ -148,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
         if (mid_player == null) {
             mid_player = MediaPlayer.create(this, R.raw.mid_freak);
             mid_player.setLooping(true);
-            float mid_volume = (high_volume + bass_volume) / 2;
+            float mid_volume = Math.min(high_volume, bass_volume);
             mid_player.setVolume(mid_volume, mid_volume);
 
         }
@@ -208,6 +214,10 @@ public class MainActivity extends AppCompatActivity {
         this.speed = speed;
         final TextView textView = findViewById(R.id.textSpeed);
         textView.setText(String.valueOf((int)(speed)));
+    }
+
+    public int getSeconds() {
+        return seconds;
     }
 
     public double getHeartRate() {
@@ -286,7 +296,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void stop(View v) {
-        playPause(v);
+        if (streaming){
+            playPause(v);
+        }
         stopPlayer();
     }
 
